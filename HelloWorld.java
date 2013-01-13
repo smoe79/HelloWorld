f4ce69c5ae0656dacf4d09811c808b0616a22c7f
Tacksoo Im <tacksoo.im@gmail.com>
2013-01-13 12:43:21 -0500

    hello world modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,13 @@ public class HelloWorld {
    public static void main(String[] args)
    {
       System.out.println("Hello World");
-	System.out.println("I just added this line in.");
+	  System.out.println("I just added this line in.");
       System.out.println("Felegh has edited this file");
 
       System.out.println("How are you??");
       System.out.println("Its me Dewguzzler");
+
+      System.out.println("Way hey up he goes and...");
    }
 
 
